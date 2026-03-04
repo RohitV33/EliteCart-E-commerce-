ef346f85d8c7729a0b8b74c7aa631c694b989c12
RohitVerma <verma61421st@gmail.com>
2026-03-04 15:19:25 +0530

    fix port for render

--- Backend/index.js
+++ Backend/index.js
@@ -94,6 +94,8 @@ app.post("/api/products",adminAuth, async (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
